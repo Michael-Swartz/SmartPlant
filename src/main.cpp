78adee728d366f581ea8c5d17accb021d9e902a7
Michael Swartz <mswartz91095@outlook.com>
2023-08-13 10:33:41 -0400

    esp32 working w/ battery

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-const char* mqtt_broker = "10.0.0.157";
+const char* mqtt_broker = "10.0.0.25";
 const char* humTopic = "/office/humidity";
 const char* tempTopic = "/office/temperature";
 const char* voltageTopic = "/office/voltage";
@@ -16,12 +16,12 @@ const int mqtt_port = 1883;
 const char* ssid = SECRET_SSID;
 const char* pass = SECRET_PASS;
 const float max_voltage = 4.01; // R1 150 R2 560
-const float max_adc_value = 1023.00;
+const float max_adc_value = 4095.00;
 
-const int analogInPin = A0;
+const int analogInPin = 32;
 float voltage = 0;
 
-#define DHTPIN 12
+#define DHTPIN 19
 #define DHTTYPE DHT22
 
 #define MSG_BUFFER_SIZE	(50)
@@ -50,26 +50,26 @@ void reconnect() {
   }
 }
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, pass);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.println(WiFi.localIP());
-}
 
 void setup() {
 
   Serial.begin(9600);
   Serial.println("Starting up...");
 
-  initWiFi;
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
 
   Serial.println();
-
+  delay(5000);
   Serial.println("Connected, IP Address: ");
   Serial.println(WiFi.localIP());
 
@@ -82,46 +82,46 @@ void setup() {
 
 void loop() {
 
-  // if (!client.connected()){
-  //   reconnect();
-  // }
+  if (!client.connected()){
+    reconnect();
+  }
 
-  // voltage = ((float(analogRead(analogInPin)) / max_adc_value) * max_voltage);
-  // Serial.print("Voltage: ");
-  // Serial.println(voltage);
-  // Serial.println(analogRead(analogInPin));
-  // snprintf(volt_msg, MSG_BUFFER_SIZE, "%f", voltage);
-  // if (client.publish(voltageTopic, volt_msg)) {
-  //   Serial.println("SUCCESS");
-  // } else {
-  //   Serial.println("FAILURE");
-  // }
+  voltage = ((float(analogRead(analogInPin)) / max_adc_value) * max_voltage);
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  Serial.println(analogRead(analogInPin));
+  snprintf(volt_msg, MSG_BUFFER_SIZE, "%f", voltage);
+  if (client.publish(voltageTopic, volt_msg)) {
+    Serial.println("SUCCESS");
+  } else {
+    Serial.println("FAILURE");
+  }
 
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  // float f_temp = dht.readTemperature(true);
-  // Serial.print("Temperature: ");
-  // snprintf(temp_msg, MSG_BUFFER_SIZE, "%f", f_temp);
-  // Serial.println(temp_msg);
+  //Read temperature as Fahrenheit (isFahrenheit = true)
+  float f_temp = dht.readTemperature(true);
+  Serial.print("Temperature: ");
+  snprintf(temp_msg, MSG_BUFFER_SIZE, "%f", f_temp);
+  Serial.println(temp_msg);
 
-  // if (client.publish(tempTopic, temp_msg)) {
-  //   Serial.println("SUCCESS");
-  // } else {
-  //   Serial.println("FAILURE");
-  // }
+  if (client.publish(tempTopic, temp_msg)) {
+    Serial.println("SUCCESS");
+  } else {
+    Serial.println("FAILURE");
+  }
 
   // // Read temperature as Fahrenheit (isFahrenheit = true)
-  // float f_hum = dht.readHumidity();
-  // Serial.print("Humidity: ");
-  // snprintf(hum_msg, MSG_BUFFER_SIZE, "%f", f_hum);
-  // Serial.println(hum_msg);
+  float f_hum = dht.readHumidity();
+  Serial.print("Humidity: ");
+  snprintf(hum_msg, MSG_BUFFER_SIZE, "%f", f_hum);
+  Serial.println(hum_msg);
 
-  // if (client.publish(humTopic, hum_msg)) {
-  //   Serial.println("SUCCESS");
-  // } else {
-  //   Serial.println("FAILURE");
-  // }
+  if (client.publish(humTopic, hum_msg)) {
+    Serial.println("SUCCESS");
+  } else {
+    Serial.println("FAILURE");
+  }
 
-  Serial.println("Going to sleep now...");
-  delay(100000);
+  //Serial.println("Going to sleep now...");
+  delay(1000);
   //ESP.deepSleep(60e6);
 }
