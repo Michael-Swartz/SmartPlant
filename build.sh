d6c2df048ddde7960418ffd851cb3ba7d57142d7
Michael Swartz <mswartz91095@outlook.com>
2023-08-13 07:28:53 -0400

    Update build script

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # Will build and push code to esp8266 using the
 # platformio.ini file as the env source.
 
+# Ensure we have permissions to the USB device
+sudo chmod a+rw /dev/ttyUSB0
+
+# Source the Platform IO venv
 . ~/.platformio/penv/bin/activate
 
 # Make sure we have all the libraries needed
